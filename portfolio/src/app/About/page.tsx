38a758e7088f03cd79ce51ee7d14e14a7115c9fa
Aquasouleon <cgottwaldt@wisc.edu>
2024-11-13 19:11:32 -0600

    Fix apostrophe

--- portfolio/src/app/About/page.tsx
+++ portfolio/src/app/About/page.tsx
@@ -26,7 +26,7 @@ return (
     </div>
     <div>
       <p>I made this website with React, Next.js, ShadCN, Typescript, and soon to be MDX and Tailwind. It&apos;s</p>
-      <p>Here's where you can see my code: <a href="https://github.com/ChristopherGottwaldt/portfolio">GitHub Repo</a></p> 
+      <p>Here&apos;s where you can see my code: <a href="https://github.com/ChristopherGottwaldt/portfolio">GitHub Repo</a></p> 
     </div>
 
     {/* <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">

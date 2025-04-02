6722267fee9c97947b82686cc02a1e44824e6cb3
Aquasouleon <cgottwaldt@wisc.edu>
2025-04-01 21:15:18 -0500

    Remove artwork from blog

--- portfolio/src/app/Blog/page.tsx
+++ portfolio/src/app/Blog/page.tsx
@@ -1,21 +1,9 @@
 import React from "react";
-import { Card, CardContent, CardHeader, CardFooter, CardTitle } from "@/components/ui/card";
-import Image from "next/image";
+// import { Card, CardContent, CardHeader, CardFooter, CardTitle } from "@/components/ui/card";
+// import Image from "next/image";
 import TestClientComponent from "@/components/blogs/TestClientComponent"; // Import the client-side MDX component
 import Navbar from "@/components/ui/NavBar";
 import { ModeToggle } from "@/components/ui/darkThemeToggle";
-
-const artworks = [
-  "cliffjumpingFriends.jpg",
-  "dragonEye.jpg",
-  "waterfallCave.jpg",
-  "abstractDepth.jpg",
-  "angelBoy.jpg",
-  "bTreeNeuron.jpg",
-  "pixelTree.png",
-  "stillLife.jpg",
-  "crystals.jpg",
-];
 
 const Home: React.FC = () => {
   return (
@@ -27,38 +15,11 @@ const Home: React.FC = () => {
         </div>
       </div>
 
-      <div className="w-full max-w-5xl text-center">
-        <br />
-        <br />
-        <br />
-        <p className="text-lg font-semibold mb-8">Some artwork that I have done!</p>
-
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-8">
-          {artworks.map((img, index) => (
-            <Card key={index} className="overflow-hidden shadow-lg">
-              <CardHeader>
-                <CardTitle>{img.slice(0, -4)}</CardTitle>
-              </CardHeader>
-              <CardContent className="p-0">
-                <Image
-                  src={`/assets/${img}`}
-                  width={500}
-                  height={500}
-                  alt={img}
-                  className="rounded-lg object-cover w-full h-auto"
-                />
-              </CardContent>
-              <CardFooter className="text-sm text-gray-500"></CardFooter>
-            </Card>
-          ))}
-        </div>
-
-        <br />
-        <p>NOTE: References from Google Images used for dragonEye, waterfallCave, and angelBoy!</p>
-      </div>
-
       {/* Render the MDX content with prose for proper styling */}
       <div className="prose lg:prose-xl dark:prose-invert">
+        <br />
+        <br />
+        <br />
         <TestClientComponent />
       </div>
     </main>
